8a7e857ac482cd41eb63a1c994728933e4e11f54
Jacob Schmitz <jake.schmitz101@gmail.com>
2014-10-24 17:04:26 -0400

    pulling links from pages with jquery

--- summarwiki/summarwiki.js
+++ summarwiki/summarwiki.js
@@ -1,6 +1,12 @@
 alert('Hello World');
 var links = [];
 $("a").each(function() {
-  links.push( this.href);
-})
+  var l = {};
+  l.link = this.href;
+  l.text = this.innerHTML;
+  console.log(l);
+  links.push(l);
+});
+
+
 
