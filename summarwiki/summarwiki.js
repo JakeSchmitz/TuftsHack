7fd96f18fad68e88a3e4912cbdf43323702e13a2
Jacob Schmitz <jake.schmitz101@gmail.com>
2014-10-24 17:14:49 -0400

    splitting work from here

--- summarwiki/summarwiki.js
+++ summarwiki/summarwiki.js
@@ -1,12 +1,12 @@
-alert('Hello World');
 var links = [];
 $("a").each(function() {
   var l = {};
   l.link = this.href;
   l.text = this.innerHTML;
-  console.log(l);
   links.push(l);
+  $(this).hover(function(){
+    console.log("fetching info for " + l.text + " from " + l.link);
+  });
 });
-
 
 
